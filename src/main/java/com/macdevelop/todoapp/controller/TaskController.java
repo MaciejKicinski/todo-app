3adf2abd4401478aecc40e50c749ad0936ac9157
Maciej Kiciński <maciej.kicinski@ttpsc.pl>
2022-01-04 18:39:19 +0100

    daily commit

--- src/main/java/com/macdevelop/todoapp/controller/TaskController.java
+++ src/main/java/com/macdevelop/todoapp/controller/TaskController.java
@@ -1,11 +1,16 @@
 package com.macdevelop.todoapp.controller;
 
+import com.macdevelop.todoapp.model.Task;
 import com.macdevelop.todoapp.model.TaskRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Slf4j
 @AllArgsConstructor
@@ -13,9 +18,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 class TaskController {
     private final TaskRepository repository;
 
-    @GetMapping("/tasks")
-    ResponseEntity<?> readAllTasks() {
+    @GetMapping(value = "/tasks", params = {"!sort", "!page", "!size"})
+    ResponseEntity<List<Task>> readAllTasks() {
         log.warn("Exposing all the tasks!");
         return ResponseEntity.ok(repository.findAll());
+    }
+
+    @GetMapping(value = "/tasks")
+    ResponseEntity<List<Task>> readAllTasks(Pageable page) {
+        log.info("Custom pageable");
+        return ResponseEntity.ok(repository.findAll(page).getContent());
     }
 }
--- src/main/java/com/macdevelop/todoapp/model/Task.java
+++ src/main/java/com/macdevelop/todoapp/model/Task.java
@@ -5,7 +5,7 @@ import javax.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "tasks")
-class Task {
+public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
