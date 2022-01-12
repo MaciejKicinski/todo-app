5fd49bce19d94b3e6c0e4a12e511bfaed1e9c344
Maciej Kiciński <maciej.kicinski@ttpsc.pl>
2022-01-12 23:56:07 +0100

    1. adding BaseAuditableEntity

---
+++ src/main/java/com/macdevelop/todoapp/model/BaseAuditableEntity.java
@@ -0,0 +1,22 @@
+package com.macdevelop.todoapp.model;
+
+import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public class BaseAuditableEntity {
+    private LocalDateTime createdOn;
+    private LocalDateTime updatedOn;
+
+    @PrePersist
+    void prePersist() {
+        createdOn = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    void preMerge() {
+        updatedOn = LocalDateTime.now();
+    }
+}
--- src/main/java/com/macdevelop/todoapp/model/Task.java
+++ src/main/java/com/macdevelop/todoapp/model/Task.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Entity
 @Table(name = "tasks")
-public class Task {
+public class Task extends BaseAuditableEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
@@ -18,8 +18,6 @@ public class Task {
     private String description;
     private boolean done;
     private LocalDateTime deadline;
-    private LocalDateTime createdOn;
-    private LocalDateTime updatedOn;
 
     public int getId() {
         return id;
@@ -59,13 +57,5 @@ public class Task {
         deadline = source.deadline;
     }
 
-    @PrePersist
-    void prePersist() {
-        createdOn = LocalDateTime.now();
-    }
 
-    @PreUpdate
-    void preMerge() {
-        updatedOn = LocalDateTime.now();
-    }
 }
